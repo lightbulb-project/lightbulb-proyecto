c810255a55f59c17e1c7a4745d5db1f4ab81b2c0
lightbulb-project <40770759+lightbulb-project@users.noreply.github.com>
2018-07-18 10:48:53 -0400

    modificación de prueba 2.2 holasss

--- app/User.php
+++ app/User.php
@@ -26,7 +26,5 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-    protected $hola = [
-        'hola', 'hola 2', 'hola 3'
-    ];
+
 }
